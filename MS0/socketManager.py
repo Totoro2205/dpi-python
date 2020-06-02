ca8fc5e0ece02529f90f17b1bf494d0de40433e0
Bruno Alexander <brualexander@gmail.com>
2020-06-02 16:49:07 -0400

    Fixed getaddrinfo

--- MS0/socketManager.py
+++ MS0/socketManager.py
@@ -19,7 +19,7 @@ class ServerSocketManager(SocketManager):
                 socket.AF_UNSPEC,
                 socket.SOCK_STREAM,
                 0,
-                socket.AI_PASSIVE):
+                int(socket.AI_PASSIVE)):
             af, socktype, proto, canonname, sa = res
 
             try:
