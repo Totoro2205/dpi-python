0cf495430ae3851c8591c36deb15ddf697c98ce8
Bruno Alexander <brualexander@gmail.com>
2020-06-09 15:11:05 -0400

    Print fix

--- MS0/concurrencyManager.py
+++ MS0/concurrencyManager.py
@@ -11,6 +11,8 @@ class ConcurrencyManager():
         while True:
             try:
                 pid, status = os.waitpid(-1, os.WNOHANG)
+                # The print on signal handler is a known bug that causes runtime errors, hence it has been commented out
+                # https://bugs.python.org/issue24283
                 #print(
                     #"Child {0} terminated with status {1}".format(
                         #pid, status))
