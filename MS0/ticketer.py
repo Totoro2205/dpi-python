821434346975777f923eb7b2935b69433a9d4fec
Bruno Alexander <brualexander@gmail.com>
2020-06-09 15:52:12 -0400

    Comments fixed

--- MS0/ticketer.py
+++ MS0/ticketer.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
 # ==============================================================================
-#   Assignment:  Milestone 1
+#   Assignment:  Milestone 2
 #
 #       Author:  Bruno Alexander Cremonese de Morais
-#     Language:  Python, using argparse, datetime and random libraries
+#     Language:  Python, using argparse, datetime, os, signal and random libraries
 #   To Compile:  python3 .\ticketer.py [-h] [-port PORT]
 #
 #        Class:  DPI 912: Python for Programmers: Sockets and Security
 #    Professor:  Harvey Kaduri
-#     Due Date:  June 2nd, 2020
-#    Submitted:  June 2nd, 2020
+#     Due Date:  June 9th, 2020
+#    Submitted:  June 9th, 2020
 #
 # -----------------------------------------------------------------------------
 #
 #    Description:  This program generates tickets following the business rules of OLG's Lotto Max using Quick Pick, Pick Your Own and Encore's logic. It is a "daemon" that processes
-#                   client requests for tickets and sends them back to the clients for saving and display
+#                   client requests for tickets and sends them back to the clients for saving and display. This program has also high availability and spawns "child" processes to handle
+#                   user connections and their requests simultaneously.
 #
 #    Input:  The program requires the port on which the daemon will listen to for setup, by default it will listen on port 5111
 #
@@ -25,7 +26,7 @@
 #                  random ticket numbers are generated for the subsequent ticketLines using the numbers on the pool for the whole ticket on Quick Pick tickets using the random library.
 #                  The algorithm also validates user inputs against invalid values that are outside the pool and/or are not integers, or duplicates.
 #                  CLI switches are used to give users options for ticket generation and process the values that the users will input for Pick Your Own tickets on the client side creating the request
-#                  The request is then processed by the daemon, sent back to the client and deserialized for display and saving it to a file.
+#                  The request is then processed by the daemon's "children", sent back separately to the clients and deserialized for display and saving it to a file.
 #
 #   Required Features Not Included:  None
 #
@@ -36,7 +37,6 @@
 # ==============================================================================
 
 import argparse
-import errno
 import os
 import signal
 from ticket import LottoTicket, QuickPick, PickYourOwn, LottoSixFortyNine, Lottario, LottoMax, TicketRequest
